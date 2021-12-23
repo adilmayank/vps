913bf0aec526685e58de62af002b59778831ac3b
adilmayank <94361109+adilmayank@users.noreply.github.com>
2021-12-23 21:45:47 +0530

    Update testing_1.py

--- testing_1.py
+++ testing_1.py
@@ -1,44 +1,63 @@
 from bs4 import BeautifulSoup
 import pandas as pd
 
-#url provides a link for local html file for scraping.
-url = r"INPUT_FILE.html"
-page = open(url)
+url = r"C:\IPNUT_FILE.html"
+file = open(url)
 
-soup = BeautifulSoup(page.read(), "html.parser")
-
-contact_name = soup.find_all("td", {"class": "contactName"})
-contact_address = soup.find_all("td", {"class": "contactAddress"})
-contact_number = soup.find_all("td", {"class": "phoneNumber"})
-
-customer_details = {"First_name": [],
-                    "Last_name": [],
+customer_details = {"First Name": [],
+                    "Last Name": [],
                     "Address": [],
-                    "Phone_Number": [],
-                    "DNCR_IND": []}
+                    "Phone Number": [],
+                    "DNCR_IND": [],
+                    "Bedrooms": [],
+                    "Bathrooms": [],
+                    "Car Space": []}
 
-for (nam, add, num) in zip(contact_name, contact_address, contact_number):
-    name = nam.text.strip().split()
-    fName = name[0]
-    lName = name[1]
-    address = add.text.strip()
-    number = num.text.strip().split()
-    number = "".join(number)
+soup = BeautifulSoup(file, "html.parser")
 
-    if len(number) != 0:
-        customer_details["First_name"].append(fName)
-        customer_details["Last_name"].append(lName)
-        customer_details["Address"].append(address)
-        if number[0] == "^":
-            customer_details["DNCR_IND"].append("Y")
-        else:
-            customer_details["DNCR_IND"].append("N")
+summaryListItems = soup.find_all("div", {"class": "summaryListItem"})
+for i in range(len(summaryListItems)):
+    content = summaryListItems[i].find("div", {"class": "content"})
+    heading = summaryListItems[i].find("ul", {"class": "iconContainer"})
+    attribute_list = heading.text.strip().split()
+    content_text = content.text
 
-        to_replace = ")^("
-        for character in to_replace:
-            number = number.replace(character, "")
+    if content_text.strip() != "No information available.":
 
-        customer_details["Phone_Number"].append(number)
+        contact_name = content.find_all("td", {"class": "contactName"})
+        contact_address = content.find_all("td", {"class": "contactAddress"})
+        contact_number = content.find_all("td", {"class": "phoneNumber"})
 
-print(customer_details) # only for confirmation purposes
-customer_details_df = pd.DataFrame(customer_details).to_excel("OUTPUT_FILE.xlsx", header=True) #prints an excel file from pandas DataFrame.
+
+        for (nam, add, num) in zip(contact_name, contact_address, contact_number):
+            name = nam.text.strip().split()
+            fName = name[0]
+            lName = name[1]
+            address = add.text.strip()
+            number = num.text.strip().split()
+            number = "".join(number)
+            contact_bedroom = attribute_list[0]
+            contact_bathroom = attribute_list[1]
+            contact_car_space = attribute_list[2]
+
+            if len(number) != 0:
+
+                customer_details["First Name"].append(fName)
+                customer_details["Last Name"].append(lName)
+                customer_details["Address"].append(address)
+                if number[0] == "^":
+                    customer_details["DNCR_IND"].append("Y")
+                else:
+                    customer_details["DNCR_IND"].append("N")
+
+                to_replace = ")^("
+                for character in to_replace:
+                    number = number.replace(character, "")
+
+                customer_details["Phone Number"].append(number)
+                customer_details["Bedrooms"].append(contact_bedroom)
+                customer_details["Bathrooms"].append(contact_bathroom)
+                customer_details["Car Space"].append(contact_car_space)
+
+customer_details_df = pd.DataFrame(customer_details).to_csv("OUTPUT_FILE.csv", header=True, index=False)
+
