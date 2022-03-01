9168af113d13c6b5bf27803c48da0dfa0ccad719
adilmayank <94361109+adilmayank@users.noreply.github.com>
2022-03-01 11:11:01 +0530

    added comments in scraper.py

--- scraper.py
+++ scraper.py
@@ -56,19 +56,19 @@ class Scrape:
                         customer_details["Phone Number"].append(contact_number_proper)
                         customer_details["DNCR_IND"].append(DNCR_IND)
 
-            pd.DataFrame(customer_details).to_csv(f"E:/rp_scraping/test.csv",
+            pd.DataFrame(customer_details).to_csv(f"E:/rp_scraping/test.csv",               #change test.csv to appropriate output file or rename it afterwords
                                                   header=header,
                                                   index=False,
                                                   mode="a")
 
-        first_file = path + rf"\3023_customer_{1}.html"
+        first_file = path + rf"\3023_customer_{1}.html"                                     #change 3023 to appropriate postal code during scraping
         scrape_details(first_file, header=True)
         if (number_of_files > 1):
             print(1, end=" ")
 
             for file_number in range(2, number_of_files + 1):
                 try:
-                    subsequent_file = path + rf"\3023_customer_{file_number}.html"
+                    subsequent_file = path + rf"\3023_customer_{file_number}.html"          #change 3023 to appropriate postal code during scraping
                     scrape_details(subsequent_file, header=False)
                     print(file_number, end=" ")
                 except:
